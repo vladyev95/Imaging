105dcf6a9df9b154e486085356eadfd17d51cafe
Vladimir Yevseenko <yevseenko.vladimir@gmail.com>
2017-04-24 23:58:01 -0400

    Forgot to add some other files in previous commit

---
+++ src/kernels.c
@@ -0,0 +1,60 @@
+#include "kernels.h"
+
+int identity_kernel[IDENTITY_KERNEL_ROWS][IDENTITY_KERNEL_COLS] =
+{ {0, 0, 0},
+  {0, 1, 0},
+  {0, 0, 0} };
+
+int box_blur_kernel[BOX_BLUR_KERNEL_ROWS][BOX_BLUR_KERNEL_COLS] =
+{ {1, 1, 1},
+  {1, 1, 1},
+  {1, 1, 1} };
+
+int edge_1_kernel[EDGE_1_KERNEL_ROWS][EDGE_1_KERNEL_COLS] =
+{ {1, 0, -1},
+  {0, 0, 0},
+  {-1, 0, 1} };
+
+
+int edge_2_kernel[EDGE_2_KERNEL_ROWS][EDGE_2_KERNEL_COLS] =
+{ {0, 1, 0},
+  {1, -4, 1},
+  {0, 1, 0} };
+
+
+int edge_3_kernel[EDGE_3_KERNEL_ROWS][EDGE_3_KERNEL_COLS] =
+{ {-1, -1, -1},
+  {-1, 8, -1},
+  {-1, -1, -1} };
+
+int sharpen_kernel[SHARPEN_KERNEL_ROWS][SHARPEN_KERNEL_COLS] =
+{ {0, -1, 0},
+  {-1, 5, -1},
+  {0, -1, 0} };
+
+int gaussian_3_3_kernel[GAUSSIAN_3_3_KERNEL_ROWS][GAUSSIAN_3_3_KERNEL_COLS] =
+{ {1, 2, 1},
+  {2, 4, 2},
+  {1, 2, 1} };
+
+int gaussian_5_5_kernel[GAUSSIAN_5_5_KERNEL_ROWS][GAUSSIAN_5_5_KERNEL_COLS] =
+{ {1, 4, 6, 4, 1},
+  {4, 16, 24, 16, 4},
+  {6, 24, 36, 24, 6},
+  {4, 16, 24, 16, 4},
+  {1, 4, 6, 4, 1} };
+
+int gradient_x_kernel[GRADIENT_X_KERNEL_ROWS][GRADIENT_X_KERNEL_ROWS] =
+{ {1, 0, -1},
+  {2, 0, -2},
+  {1, 0 -1} };
+
+int gradient_y_kernel[GRADIENT_Y_KERNEL_ROWS][GRADIENT_X_KERNEL_ROWS] =
+{ {1, 2, 1},
+  {0, 0, 0},
+  {-1, -2, -1} };
+
+int laplace_kernel[LAPLACE_KERNEL_ROWS][LAPLACE_KERNEL_COLS] =
+{ {0, -1, 0},
+  {-1, 4, -1},
+  {0, -1, 0} };
--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	char *imgp;
-	struct image *img, *gx, *gy, *l;
+	struct image *img, *gx, *gy, *l, *gx2, *gy2;
 
 	if (argc != 2)
 		ERROR("no image specified\n");
@@ -22,15 +22,21 @@ int main(int argc, char *argv[])
 	img = read_image(imgp);
 	gx = gradient_x(img);
 	gy = gradient_y(img);
+	gx2 = gradient2_x(img);
+	gy2 = gradient2_y(img);
 	l = laplace(img);
 			
 	write_image(gx, "../images/gx.pgm");
 	write_image(gy, "../images/gy.pgm");
+	write_image(gx2, "../images/gx2.pgm");
+	write_image(gy2, "../images/gy2.pgm");
 	write_image(l, "../images/l.pgm");
 	
 	free_image(img);
 	free_image(gx);
 	free_image(gy);
+	free_image(gx2);
+	free_image(gy2);
 	free_image(l);
 	
 	
