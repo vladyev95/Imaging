45d535888b1c28cdec6b2b388f6948b2530be78f
Harrison <harrison.termotto@stonybrook.edu>
2017-04-26 13:28:02 -0400

    Temp main for testing perona malik.

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	char *imgp;
-	struct image *img, *gx, *gy, *l, *gx2, *gy2;
+	struct image *smimg, *img, *gx, *gy, *l, *gx2, *gy2, *pMalik;
 
 	if (argc != 2)
 		ERROR("no image specified\n");
@@ -20,6 +20,8 @@ int main(int argc, char *argv[])
 	imgp = argv[1];
 	
 	img = read_image(imgp);
+	smimg = convolve(img,3,3,gaussian_3_3_kernel, (1.0f/16));   
+	write_image(img, "../images/smooth.pgm");
 	gx = gradient_x(img);
 	gy = gradient_y(img);
 	gx2 = gradient2_x(img);
@@ -32,14 +34,16 @@ int main(int argc, char *argv[])
 	write_image(gy2, "../images/gy2.pgm");
 	write_image(l, "../images/l.pgm");
 	
-	free_image(img);
 	free_image(gx);
 	free_image(gy);
 	free_image(gx2);
 	free_image(gy2);
 	free_image(l);
 	
-	
+//	pMalik = run_perona_malik(img, 1, 1, 32, .25);
+//	write_image(pMalik, "../images/pmalik.pgm");
+//	free(img);
+//	free(pMalik);
 	
 	return EXIT_SUCCESS;
 }
