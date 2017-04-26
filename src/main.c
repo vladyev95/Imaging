db4b0f3594e88d5e10e7553be2a5149fcef653d7
Vladimir Yevseenko <yevseenko.vladimir@gmail.com>
2017-04-26 16:35:46 -0400

    Improved main

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "my_error.h"
 #include "lee_filter.h"
@@ -9,31 +10,79 @@
 #include "median_filter.h"
 #include "anisotropic.h"
 
+
+/*
+ * turns "/path/to/file" into "/path/to/file_type", places in buf
+ */
+void write_path(char *buf, const char *imgp, const char *type)
+{
+	strcpy(buf, imgp);
+	strcat(buf, "_");
+	strcat(buf, type);
+	strcat(buf, ".pgm");
+}
+
 int main(int argc, char *argv[])
 {
-	char *imgp;
-	struct image *img, *gx, *gy, *l, *gx2, *gy2;
+	char *imgp, buf[256];
+	struct image *img, *smooth, *gx, *gy, *gx2, *gy2, *l,
+			*smooth_gx, *smooth_gy, *smooth_l, *smooth_gx2,
+			*smooth_gy2;
 
 	if (argc != 2)
 		ERROR("no image specified\n");
 	
 
 	imgp = argv[1];
-
 	img = read_image(imgp);
+	imgp[strlen(imgp) - 4] = '\0';
+	smooth = convolve(img, 3, 3, gaussian_3_3_kernel, 
+			GAUSSIAN_3_3_KERNEL_COEFF);
 	gx = gradient_x(img);
 	gy = gradient_y(img);
 	gx2 = gradient2_x(img);
 	gy2 = gradient2_y(img);
 	l = laplace(img);
-			
-	write_image(gx, "../images/gx.pgm");
-	write_image(gy, "../images/gy.pgm");
-	write_image(gx2, "../images/gx2.pgm");
-	write_image(gy2, "../images/gy2.pgm");
-	write_image(l, "../images/l.pgm");
 
-	print_image(gx);
+	smooth_gx = gradient_x(smooth);
+	smooth_gy = gradient_y(smooth);
+	smooth_l = laplace(smooth);
+	smooth_gx2 = gradient2_x(smooth);
+	smooth_gy2 = gradient2_y(smooth);
+
+			
+	write_path(buf, imgp, "smooth");
+	write_image(smooth, buf);
+	
+	write_path(buf, imgp, "gx");
+	write_image(gx, buf);
+
+	write_path(buf, imgp, "gy");
+	write_image(gy, buf);
+
+	write_path(buf, imgp, "gx2");
+	write_image(gx2, buf);
+
+	write_path(buf, imgp, "gy2");
+	write_image(gy2, buf);
+
+	write_path(buf, imgp, "l");
+	write_image(l, buf);
+
+	write_path(buf, imgp, "smooth_gx");
+	write_image(smooth_gx, buf);
+
+	write_path(buf, imgp, "smooth_gy");
+	write_image(smooth_gy, buf);
+
+	write_path(buf, imgp, "smooth_l");
+	write_image(smooth_l, buf);
+
+	write_path(buf, imgp, "smooth_gx2");
+	write_image(smooth_gx2, buf);
+
+	write_path(buf, imgp, "smooth_gy2");
+	write_image(smooth_gy2, buf);
 	
 	free_image(img);
 	free_image(gx);
